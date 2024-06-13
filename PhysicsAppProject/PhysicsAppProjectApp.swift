38d6d7b7a24f45405b76ac2fab524863b1099420
Surya Arish <suryaarish95@gmail.com>
2024-06-13 11:18:33 +0530

    Initial Commit

---
+++ PhysicsAppProject/PhysicsAppProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhysicsAppProjectApp.swift
+//  PhysicsAppProject
+//
+//  Created by Surya on 13/06/24.
+//
+
+import SwiftUI
+
+@main
+struct PhysicsAppProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
